e413df91918723b04aa6b3776d6c4be24911448d
oliver <weiler@predic8.de>
2017-11-17 11:43:34 +0100

    Fix test data

--- bootstrap/perform-workflow.sh
+++ bootstrap/perform-workflow.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# TODO catalogue service
+
 # 1. Perform checkout request
+
+echo 'Performing checkout request'
 
 basket_uuid=$(curl --request POST \
   --url http://localhost:8081/checkouts \
@@ -11,7 +15,7 @@ basket_uuid=$(curl --request POST \
   "items": [
     {
       "article": "cab08236-1924-4f90-975d-9e056ed14c48",
-      "quantity": 300
+      "quantity": 3
     },
     {
       "article": "d996413a-21fb-4c12-b4a8-64996876505b",
@@ -20,12 +24,9 @@ basket_uuid=$(curl --request POST \
   ]
 }' | jq -r .basket)
 
-if (($? != 0 )); then
-    echo "Checkout failed"
-    exit
-fi
-
 # 2. Perform rating request
+
+echo "Performing payment request with basket id $basket_uuid"
 
 curl --request POST \
   --url "http://localhost:8082/payments/$basket_uuid" \
