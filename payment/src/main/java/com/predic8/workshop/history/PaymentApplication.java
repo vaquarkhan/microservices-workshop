485630bf31edd03c1a33e5218b88d0fe76770fb4
oliver <weiler@predic8.de>
2017-11-24 14:50:36 +0100

    Fix API gateway configuration

--- payment/src/main/java/com/predic8/workshop/history/PaymentApplication.java
+++ payment/src/main/java/com/predic8/workshop/history/PaymentApplication.java
@@ -3,6 +3,7 @@ package com.predic8.workshop.history;
 import com.predic8.workshop.history.dto.Payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentApplication {
 	@Bean
--- rating/src/main/java/de/predic8/workshop/rating/RatingApplication.java
+++ rating/src/main/java/de/predic8/workshop/rating/RatingApplication.java
@@ -2,12 +2,14 @@ package de.predic8.workshop.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RatingApplication {
 	@Bean
